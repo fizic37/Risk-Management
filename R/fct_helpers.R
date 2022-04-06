eaa4cb587770911c3949fdfa533f55e1ff0f0790
Marius Tita <marius.tita81@gmail.com>
2022-04-06 13:32:41 +0300

    Add rowname argument to dt_generate_function

--- R/fct_helpers.R
+++ R/fct_helpers.R
@@ -8,9 +8,9 @@
 
 library(magrittr)
 dt_generate_function <- function(df,shape = "compact",round_col = NULL,perc_col = NULL,caption = "",
-                                 editable=FALSE,escape=T, dom = "tp",
+                                 editable=FALSE,escape=T, dom = "tp", rownames=FALSE,
                                  digits=0,show_buttons=FALSE,digits_perc=1, pageLength = NULL) {
-  result <- DT::datatable(class = shape,data = df,rownames = FALSE, escape=escape,
+  result <- DT::datatable(class = shape,data = df,rownames = rownames, escape=escape,
                           caption = htmltools::tags$caption(style = 'caption-side: top; text-align: left;',
                                           caption),editable=editable,extensions = "Buttons",
                           options = list(pageLength = pageLength,
